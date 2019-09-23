84b755bd560f77309c588cbde40a5c7d88ac1bd1
Dan MacDonagh <dpmacdonagh@gmail.com>
2019-09-23 13:58:50 -0600

    Initial Launcher Setup

---
+++ Assets/Scripts/LauncherManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+namespace Com.TDS {
+  public class LauncherManager : MonoBehaviourPunCallbacks {
+    [SerializeField]
+    private byte maxPlayersPerRoom = 4;
+
+    [SerializeField]
+    private GameObject controlPanel;
+
+    [SerializeField]
+    private GameObject connectionStatus;
+
+    string gameVersion = "1";
+    bool isConnecting;
+
+    void Awake() {
+      PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start() {
+      ShowControlPanelUI();
+    }
+
+    public void Connect() {
+      isConnecting = true;
+      ShowConnectionStatusUI();
+
+      if (PhotonNetwork.IsConnected) {
+        PhotonNetwork.JoinRandomRoom();
+      } else {
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.ConnectUsingSettings();
+      }
+    }
+
+    public override void OnConnectedToMaster() {
+      if (isConnecting) {
+        PhotonNetwork.JoinRandomRoom();
+      }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+      ShowControlPanelUI();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+      PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom() {
+      if (PhotonNetwork.CurrentRoom.PlayerCount == 1) {
+        PhotonNetwork.LoadLevel("Test Arena");
+      }
+    }
+
+    void ShowControlPanelUI() {
+      controlPanel.SetActive(true);
+      connectionStatus.SetActive(false);
+    }
+
+    void ShowConnectionStatusUI() {
+      controlPanel.SetActive(false);
+      connectionStatus.SetActive(true);
+    }
+  }
+}
---
+++ Assets/Scripts/PlayerNameInput.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.TDS {
+  public class PlayerNameInput : MonoBehaviour {
+    [SerializeField]
+    private InputField inputField;
+
+    const string playerNamePrefKey = "PlayerName";
+
+    void Start() {
+      string defaultName = string.Empty;
+
+      if (PlayerPrefs.HasKey(playerNamePrefKey)) {
+        defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+        inputField.text = defaultName;
+      }
+
+      PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string value) {
+      if (string.IsNullOrEmpty(value)) {
+        return;
+      }
+
+      PhotonNetwork.NickName = value;
+      PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+  }
+}
